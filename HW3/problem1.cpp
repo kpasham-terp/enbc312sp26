36ead2f40a74b3f87ac379d54728ab79509202b9
kpasham-terp <kpasham@terpmail.umd.edu>
2026-02-25 00:01:56 -0500

    finished problem 1

--- HW3/problem1.cpp
+++ HW3/problem1.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int placeholder = arr[1];
+    *(arr+1) = arr[3];
+    *(arr+3) = placeholder;
+    for(int i = 0; i < 5; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
